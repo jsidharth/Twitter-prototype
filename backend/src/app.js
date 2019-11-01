4b7ea1c71b5bd46c5a483001998d5cff98063943
Sidharth J <sidharthjayaprakash93@gmail.com>
2019-11-01 10:10:09 -0700

    Modified boiler plate to include kafka

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,5 @@
 //import the require dependencies
-import express from "express";
+import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import Promise from 'bluebird';
@@ -8,27 +8,34 @@ import cors from 'cors';
 const app = express();
 
 //use cors to allow cross origin resource sharing. Take value from .env file
-app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  }),
+);
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+);
 app.use(bodyParser.json());
 
 //Allow Access Control
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers',
+  );
   res.setHeader('Cache-Control', 'no-cache');
   next();
 });
 
 //start your server on port 3001
 app.listen(3001);
-console.log("Server Listening on port 3001");
+console.log('Server Listening on port 3001');
 module.exports = app;
