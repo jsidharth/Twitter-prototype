1c0c8c0ab5bed05a11fbce6ae97f1e15a7b22f31
Madhu PK <54852226+MadhuPrasannaK@users.noreply.github.com>
2019-11-12 18:34:59 -0800

    Feat/sidebar (#39)
    * Sidebar added. Font style needs to changed eventually
    
    * Package json dependencies modified
    
    * Sidebar code modified as per AirBnB styling
    
    * Changes done as per comments
    
    * Sidebar icon modifications
    
    * Styling changes
    
    * Sidebar complete

---
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,102 @@
+import React, { Component } from 'react';
+import { Navbar, ListGroup } from 'react-bootstrap';
+import './Sidebar.css';
+import NavLink from 'react-bootstrap/NavLink';
+import { FaRegUserCircle } from 'react-icons/fa';
+import { GoHome, GoMail, GoNote, GoGraph } from 'react-icons/go';
+import { IoMdNotificationsOutline } from 'react-icons/io';
+import { FiBookmark } from 'react-icons/fi';
+
+class Sidebar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="fixed">
+        <nav className="sidebar flex-column">
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <Navbar.Brand href="/home">
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/fr/thumb/c/c8/Twitter_Bird.svg/944px-Twitter_Bird.svg.png"
+                  width="55px"
+                  height="55px"
+                  className="d-inline-block align-top"
+                  alt="Main logo link to home"
+                />
+                <br />
+                <br />
+              </Navbar.Brand>
+            </ListGroup.Item>
+            <NavLink>
+              <ListGroup.Item action href="/home">
+                <div className="flexSidebars">
+                  <GoHome size={40} />
+                  <p className="sidebarTopics">Home</p>
+                </div>
+              </ListGroup.Item>
+            </NavLink>
+            <NavLink>
+              <ListGroup.Item action href="/home">
+                <div className="flexSidebars">
+                  <IoMdNotificationsOutline size={40} />
+                  <p className="sidebarTopics">Notifications</p>
+                </div>
+              </ListGroup.Item>
+            </NavLink>
+            <NavLink>
+              <ListGroup.Item action href="/home">
+                <div className="flexSidebars">
+                  <GoMail size={40} />
+                  <p className="sidebarTopics">Messages</p>
+                </div>
+              </ListGroup.Item>
+            </NavLink>
+            <NavLink>
+              <ListGroup.Item action href="/home">
+                <div className="flexSidebars">
+                  <FiBookmark size={40} />
+                  <p className="sidebarTopics">Bookmarks</p>
+                </div>
+              </ListGroup.Item>
+            </NavLink>
+            <NavLink>
+              <ListGroup.Item action href="/home">
+                <div className="flexSidebars">
+                  <GoNote size={40} />
+                  <p className="sidebarTopics">Lists</p>
+                </div>
+              </ListGroup.Item>
+            </NavLink>
+            <NavLink>
+              <ListGroup.Item action href="/home">
+                <div className="flexSidebars">
+                  <FaRegUserCircle size={40} />
+                  <p className="sidebarTopics">Profile</p>
+                </div>
+              </ListGroup.Item>
+            </NavLink>
+            <NavLink>
+              <ListGroup.Item action href="/home">
+                <div className="flexSidebars">
+                  <GoGraph size={40} />
+                  <p className="sidebarTopics">Analytics</p>
+                </div>
+              </ListGroup.Item>
+            </NavLink>
+            <NavLink>
+              <button className="tweetBtn" type="button">
+                Tweet
+              </button>
+            </NavLink>
+          </ListGroup>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default Sidebar;
