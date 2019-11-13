0d42f2019a0c7fafc54bcec87e2e2dcc68b25e57
nayakmitra7 <55523134+nayakmitra7@users.noreply.github.com>
2019-11-12 17:26:07 -0800

    User details fetch and update api (#46)
    * User details fetch and update api
    
    * Get Put details

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -1,9 +1,17 @@
 import register from '../services/user.services/register';
+import getDetails from '../services/user.services/getDetails';
+import putDetails from '../services/user.services/putDetails';
 
 const handleRequest = (payload, callback) => {
   switch (payload.action) {
     case 'USER_REGISTER':
       register.handleRequest(payload.body, callback);
+      break;
+    case 'USER_GET_DETAILS':
+      getDetails.handleRequest(payload.userId, callback);
+      break;
+    case 'USER_PUT_DETAILS':
+      putDetails.handleRequest(payload.body, callback);
       break;
     default:
       break;
