0acacd835ca8521aa51d7feb982749c3d0a88f73
Sidharth J <sidharthjayaprakash93@gmail.com>
2019-11-15 15:21:38 -0800

    Merged conflicts

--- middleware/src/routes/tweetRoutes.js
+++ middleware/src/routes/tweetRoutes.js
@@ -48,4 +48,25 @@ tweetRouter.post('/post', (req, res) => {
   );
 });
 
+tweetRouter.get('/detail/:tweetId', (req, res) => {
+  const { tweetId } = req.params;
+  kafka.makeRequest(
+    'tweetTopic',
+    {
+      tweetId,
+      action: 'TWEET_DETAIL',
+    },
+    (err, result) => {
+      if (err) {
+        console.log('Error ', err);
+        res.status(500).json({
+          message: err.message,
+        });
+      } else {
+        res.status(200).json(result);
+      }
+    }
+  );
+});
+
 export default tweetRouter;
