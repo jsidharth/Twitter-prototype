63feebb61f1ad951f1a86500ad0776d33e31dea6
tinaaggarwal <tina.14a@gmail.com>
2019-11-15 10:37:49 -0800

    post tweet ui, arranged next to Sidebar (#79)

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -15,85 +15,87 @@ class Sidebar extends Component {
 
   render() {
     return (
-      <div className="fixed">
-        <nav className="sidebar flex-column">
-          <ListGroup variant="flush">
-            <ListGroup.Item>
-              <Navbar.Brand href="/home">
-                <img
-                  src="https://upload.wikimedia.org/wikipedia/fr/thumb/c/c8/Twitter_Bird.svg/944px-Twitter_Bird.svg.png"
-                  width="55px"
-                  height="55px"
-                  className="d-inline-block align-top"
-                  alt="Main logo link to home"
-                />
-                <br />
-                <br />
-              </Navbar.Brand>
-            </ListGroup.Item>
-            <NavLink>
-              <ListGroup.Item action href="/home">
-                <div className="flexSidebars">
-                  <GoHome size={40} />
-                  <p className="sidebarTopics">Home</p>
-                </div>
+      <div>
+        <div>
+          <nav className="sidebar">
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Navbar.Brand href="/home">
+                  <img
+                    src="https://upload.wikimedia.org/wikipedia/fr/thumb/c/c8/Twitter_Bird.svg/944px-Twitter_Bird.svg.png"
+                    width="55px"
+                    height="55px"
+                    className="d-inline-block align-top"
+                    alt="Main logo link to home"
+                  />
+                  <br />
+                  <br />
+                </Navbar.Brand>
               </ListGroup.Item>
-            </NavLink>
-            <NavLink>
-              <ListGroup.Item action href="/home">
-                <div className="flexSidebars">
-                  <IoMdNotificationsOutline size={40} />
-                  <p className="sidebarTopics">Notifications</p>
-                </div>
-              </ListGroup.Item>
-            </NavLink>
-            <NavLink>
-              <ListGroup.Item action href="/home">
-                <div className="flexSidebars">
-                  <GoMail size={40} />
-                  <p className="sidebarTopics">Messages</p>
-                </div>
-              </ListGroup.Item>
-            </NavLink>
-            <NavLink>
-              <ListGroup.Item action href="/home">
-                <div className="flexSidebars">
-                  <FiBookmark size={40} />
-                  <p className="sidebarTopics">Bookmarks</p>
-                </div>
-              </ListGroup.Item>
-            </NavLink>
-            <NavLink>
-              <ListGroup.Item action href="/home">
-                <div className="flexSidebars">
-                  <GoNote size={40} />
-                  <p className="sidebarTopics">Lists</p>
-                </div>
-              </ListGroup.Item>
-            </NavLink>
-            <NavLink>
-              <ListGroup.Item action href="/home">
-                <div className="flexSidebars">
-                  <FaRegUserCircle size={40} />
-                  <p className="sidebarTopics">Profile</p>
-                </div>
-              </ListGroup.Item>
-            </NavLink>
-            <NavLink>
-              <ListGroup.Item action href="/home">
-                <div className="flexSidebars">
-                  <GoGraph size={40} />
-                  <p className="sidebarTopics">Analytics</p>
-                </div>
-              </ListGroup.Item>
-            </NavLink>
-            <NavLink>
-              <button className="tweetBtn" type="button">
-                Tweet
+              <NavLink>
+                <ListGroup.Item action href="/home">
+                  <div className="flexSidebars">
+                    <GoHome size={40} />
+                    <p className="sidebarTopics">Home</p>
+                  </div>
+                </ListGroup.Item>
+              </NavLink>
+              <NavLink>
+                <ListGroup.Item action href="/home">
+                  <div className="flexSidebars">
+                    <IoMdNotificationsOutline size={40} />
+                    <p className="sidebarTopics">Notifications</p>
+                  </div>
+                </ListGroup.Item>
+              </NavLink>
+              <NavLink>
+                <ListGroup.Item action href="/home">
+                  <div className="flexSidebars">
+                    <GoMail size={40} />
+                    <p className="sidebarTopics">Messages</p>
+                  </div>
+                </ListGroup.Item>
+              </NavLink>
+              <NavLink>
+                <ListGroup.Item action href="/home">
+                  <div className="flexSidebars">
+                    <FiBookmark size={40} />
+                    <p className="sidebarTopics">Bookmarks</p>
+                  </div>
+                </ListGroup.Item>
+              </NavLink>
+              <NavLink>
+                <ListGroup.Item action href="/home">
+                  <div className="flexSidebars">
+                    <GoNote size={40} />
+                    <p className="sidebarTopics">Lists</p>
+                  </div>
+                </ListGroup.Item>
+              </NavLink>
+              <NavLink>
+                <ListGroup.Item action href="/home">
+                  <div className="flexSidebars">
+                    <FaRegUserCircle size={40} />
+                    <p className="sidebarTopics">Profile</p>
+                  </div>
+                </ListGroup.Item>
+              </NavLink>
+              <NavLink>
+                <ListGroup.Item action href="/home">
+                  <div className="flexSidebars">
+                    <GoGraph size={40} />
+                    <p className="sidebarTopics">Analytics</p>
+                  </div>
+                </ListGroup.Item>
+              </NavLink>
+              <NavLink>
+                <button className="tweetBtn" type="button">
+                  Tweet
               </button>
-            </NavLink>
-          </ListGroup>
-        </nav>
+              </NavLink>
+            </ListGroup>
+          </nav>
+        </div>
       </div>
     );
   }
---
+++ client/src/js/actions/index.js
@@ -0,0 +1,3 @@
+import * as imageActions from './imageActions';
+
+export { imageActions };
