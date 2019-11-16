9091f5e0d181daa08810a80efd5325b24ffe5424
Savyasachi Jagadeeshan <jsavyasachi@gmail.com>
2019-11-15 17:43:26 -0800

    Search backend  (#81)
    * added search user by handle feature
    
    * added search all feature
    
    * completed search feature backend
    
    * added sort and views function in tweet results
    
    * fixed issues
    
    * fixed issues

--- server/services/search.services/searchAll.js
+++ server/services/search.services/searchAll.js
@@ -1,1 +1,91 @@
-//import Users from '../../models/user.model';
+/* eslint-disable no-underscore-dangle */
+import Promise from 'bluebird';
+import moment from 'moment';
+import Users from '../../models/user.model';
+import Tweets from '../../models/tweet.model';
+
+const handleRequest = async (searchTerm, callback) => {
+  const users = await Users.aggregate([
+    {
+      $match: {
+        name: {
+          $regex: searchTerm,
+          $options: 'i',
+        },
+      },
+    },
+    {
+      $project: {
+        name: 1,
+        bio: 1,
+        handle: 1,
+      },
+    },
+  ]);
+
+  const searchTweets = await Tweets.aggregate([
+    {
+      $match: {
+        body: {
+          $regex: searchTerm,
+          $options: 'i',
+        },
+      },
+    },
+  ]);
+  if (searchTweets && searchTweets.length) {
+    let updatedTweets = await Promise.map(searchTweets, async tweet => {
+      const user = await Users.findOne(
+        {
+          tweets: tweet._id,
+        },
+        {
+          name: 1,
+          handle: 1,
+        }
+      );
+      return {
+        _id: tweet._id,
+        name: user.name,
+        handle: user.handle,
+        likes_count: tweet.likes.length || 0,
+        comments_count: tweet.comments.length || 0,
+        retweet_count: tweet.retweets.length || 0,
+        body: tweet.body,
+        image: tweet.image,
+        created_at: tweet.created_at,
+      };
+    });
+    updatedTweets = updatedTweets.sort((first, second) =>
+      moment(second.created_at).diff(first.created_at)
+    );
+    let updateTweetViewsPromise = Promise.resolve();
+    updateTweetViewsPromise = Promise.map(updatedTweets, tweet => {
+      return Tweets.findOneAndUpdate(
+        {
+          _id: tweet._id,
+        },
+        {
+          $inc: {
+            views: 1,
+          },
+        }
+      );
+    });
+    updateTweetViewsPromise.then(() => {
+      callback(null, {
+        users,
+        tweets: updatedTweets,
+      });
+    });
+  } else {
+    callback(null, {
+      users,
+      tweets: [],
+    });
+  }
+};
+
+export default {
+  handleRequest,
+};
--- server/services/search.services/searchTopic.js
+++ server/services/search.services/searchTopic.js
@@ -1,1 +1,67 @@
-//import Users from '../../models/user.model';
+import Promise from 'bluebird';
+import moment from 'moment';
+import Users from '../../models/user.model';
+import Tweets from '../../models/tweet.model';
+
+const handleRequest = async (searchTerm, callback) => {
+  const searchTweets = await Tweets.find({
+    hashtags: {
+      $in: [searchTerm],
+    },
+  });
+  if (searchTweets && searchTweets.length) {
+    let updatedTweets = await Promise.map(searchTweets, tweet => {
+      return Users.findOne(
+        {
+          // eslint-disable-next-line no-underscore-dangle
+          tweets: tweet._id,
+        },
+        {
+          name: 1,
+          handle: 1,
+        }
+      ).then(user => {
+        return {
+          // eslint-disable-next-line no-underscore-dangle
+          _id: tweet._id,
+          name: user.name,
+          handle: user.handle,
+          likes_count: tweet.likes.length || 0,
+          comments_count: tweet.comments.length || 0,
+          retweet_count: tweet.retweets.length || 0,
+          body: tweet.body,
+          image: tweet.image,
+          created_at: tweet.created_at,
+        };
+      });
+    });
+    updatedTweets = updatedTweets.sort((first, second) =>
+      moment(second.created_at).diff(first.created_at)
+    );
+    let updateTweetViewsPromise = Promise.resolve();
+    updateTweetViewsPromise = Promise.map(updatedTweets, tweet => {
+      return Tweets.findOneAndUpdate(
+        {
+          // eslint-disable-next-line no-underscore-dangle
+          _id: tweet._id,
+        },
+        {
+          $inc: {
+            views: 1,
+          },
+        }
+      );
+    });
+    updateTweetViewsPromise.then(() => {
+      callback(null, { tweet: updatedTweets });
+    });
+  } else {
+    callback(null, {
+      tweets: [],
+    });
+  }
+};
+
+export default {
+  handleRequest,
+};
