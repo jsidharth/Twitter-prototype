e8aa1d1567b07dd08307d97489fa2042d1745302
Tina Aggarwal <tina.14a@gmail.com>
2019-11-17 16:03:57 -0800

    fetch twitter feed

--- client/src/components/TweetCard/TweetCard.js
+++ client/src/components/TweetCard/TweetCard.js
@@ -1,13 +1,12 @@
+/* eslint-disable react/prop-types */
 import React, { Component } from 'react';
-import Image from 'react-bootstrap/Image';
-import { Card, CardContent, Paper, Icon } from '@material-ui/core';
-import { InsertPhoto, ControlCamera } from '@material-ui/icons';
-// import { connect } from 'react-redux';
-// import { imageActions } from '../../js/actions/index';
+import { Card, CardContent } from '@material-ui/core';
+import { connect } from 'react-redux';
 import { FiMessageCircle, FiUpload } from 'react-icons/fi';
 import { FaRegHeart } from 'react-icons/fa';
 import { AiOutlineRetweet } from 'react-icons/ai';
 import './TweetCard.css';
+import { tweetActions } from '../../js/actions/index';
 
 class TweetCard extends Component {
   constructor(props) {
@@ -15,60 +14,44 @@ class TweetCard extends Component {
     this.state = {};
   }
 
-  render() {
-    const mock_data = [
-      {
-        _id: '5dcb33ef1c9d440000b0d338',
-        name: 'Savyasachi J',
-        handle: 'savy',
-        likes_count: 1,
-        comments_count: 0,
-        retweet_count: 0,
-        body: 'This is another tweet',
-        image: '',
-        created_at: '2019-11-11T08:00:00.000Z',
-      },
-      {
-        _id: '5dcb33ef1c9d440000b0d338',
-        name: 'xyz',
-        handle: 'xyz',
-        likes_count: 1,
-        comments_count: 2,
-        retweet_count: 3,
-        body: 'This is another tweet by meeee',
-        image: '',
-        created_at: '2019-11-13T08:00:00.000Z',
-      },
-    ];
+  componentDidMount() {
+    const data = {
+      // userId: this.props.userId
+      userId: '5dcb31841c9d440000b0d332',
+    };
+    const { fetchFeed } = this.props;
+    fetchFeed(data);
+  }
 
-    const feed = mock_data.map(data => {
-      var myDate = new Date(data.created_at);
+  render() {
+    const { feed } = this.props;
+
+    const renderFeed = feed.map(data => {
+      let myDate = new Date(data.created_at);
       myDate = myDate.toString();
       myDate = myDate.split(' ');
-      console.log(myDate);
       return (
-        <Card className="cardWidth">
+        // eslint-disable-next-line no-underscore-dangle
+        <Card className="cardWidth" key={data._id}>
           <CardContent className="cardContent">
             <div className="flexImageTweet">
               <div>
                 {/* Include user profile image if available */}
-                <Image
-                  src="/images/default_profile_bigger.png"
-                  roundedCircle
-                  className="profileImage"
-                />
+                <img src="/images/default_profile_bigger.png" className="profileImage" alt="user" />
               </div>
-              <div className="flexNameHandle">
-              <p className="tweetUserName">{data.name}</p>
-              <p className="tweetUserHandle">@{data.handle}</p>
-              <p className="tweetUserName">.</p>
-              <p className="tweetDate">
-                {myDate[1]} {myDate[2]}, {myDate[3]}
-              </p>
+              <div>
+                <div className="flexNameHandle">
+                  <p className="tweetUserName">{data.name}</p>
+                  <p className="tweetUserHandle">@{data.handle}</p>
+                  <p className="tweetUserName">.</p>
+                  <p className="tweetDate">
+                    {myDate[1]} {myDate[2]}, {myDate[3]}
+                  </p>
+                </div>
+                <p>{data.body}</p>
+                {data.image ? <img src={data.image} alt="Tweet" /> : null}
+              </div>
             </div>
-            </div>
-            
-            <p>{data.body}</p>
             <div className="flexCardBtns">
               <div className="flexBtnCnt">
                 <FiMessageCircle size={20} />
@@ -90,10 +73,21 @@ class TweetCard extends Component {
         </Card>
       );
     });
-
-    return <div>{feed}</div>;
+    return <div>{renderFeed}</div>;
   }
 }
 
-export default TweetCard;
+const mapStateToProps = state => {
+  return {
+    feed: state.tweet.feed,
+  };
+};
 
+const mapDispatchToProps = dispatch => ({
+  fetchFeed: data => dispatch(tweetActions.fetchFeed(data)),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(TweetCard);
--- client/src/js/constants/index.js
+++ client/src/js/constants/index.js
@@ -1,6 +1,7 @@
 const actionTypes = {
   UPLOAD_IMAGE: 'UPLOAD_IMAGE',
   POST_TWEET: 'POST_TWEET',
+  TWEET_FEED: 'TWEET_FEED',
 };
 
 export default actionTypes;
