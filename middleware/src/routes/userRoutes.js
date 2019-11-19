4b7c38af41eff898575dbe5c471def51a4dd1ec5
Sidharth J <sidharthjayaprakash93@gmail.com>
2019-11-18 18:45:55 -0800

    Redis caching implemented

--- middleware/src/routes/userRoutes.js
+++ middleware/src/routes/userRoutes.js
@@ -1,9 +1,19 @@
 import express from 'express';
 import passport from 'passport';
+import redis from 'redis';
+import Promise from 'bluebird';
 
+Promise.promisifyAll(redis.RedisClient.prototype);
+Promise.promisifyAll(redis.Multi.prototype);
+
+const client = redis.createClient(6379);
 const kafka = require('../../kafka/client');
 
 const userRouter = express.Router();
+
+client.on('error', err => {
+  console.log('Error ', err);
+});
 
 userRouter.post('/register', passport.authenticate('register'), (req, res) => {
   console.log('Inside POST user register');
@@ -52,23 +62,36 @@ userRouter.post('/login', passport.authenticate('login'), (req, res) => {
 userRouter.get('/details/(:data)', (req, res) => {
   console.log('Inside get user details');
   console.log('Request Body: ', req.params.data);
-  kafka.makeRequest(
-    'userTopic',
-    {
-      userId: req.params.data,
-      action: 'USER_GET_DETAILS',
-    },
-    (err, result) => {
-      if (err) {
-        console.log('Error ', err);
-        res.status(500).json({
-          message: err.message,
-        });
-      } else {
-        res.status(200).json(result);
-      }
+  const { data } = req.params;
+  // Check if user is in redis cache
+  return client.getAsync(data).then(cacheResult => {
+    // If found in cache return
+    if (cacheResult) {
+      res.status(200).json(JSON.parse(cacheResult));
+    } else {
+      // Fetch details from database
+      kafka.makeRequest(
+        'userTopic',
+        {
+          userId: data,
+          action: 'USER_GET_DETAILS',
+        },
+        (err, result) => {
+          if (err) {
+            console.log('Error ', err);
+            res.status(500).json({
+              message: err.message,
+            });
+          } else {
+            // store in cache with a timeout of 3600 sec and respond
+            return client.setexAsync(data, 3600, JSON.stringify(result)).then(() => {
+              res.status(200).json(result);
+            });
+          }
+        }
+      );
     }
-  );
+  });
 });
 
 userRouter.put('/details', (req, res) => {
