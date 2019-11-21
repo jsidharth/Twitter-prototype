d7a921b9d20cdd3eb4ba570a3a91c8d20b31bb5a
Mitra Nayak <mitragunakara.nayak@sjsu.edu>
2019-11-20 17:08:51 -0800

    populate

--- server/services/user.services/getDetails.js
+++ server/services/user.services/getDetails.js
@@ -1,13 +1,16 @@
 import Users from '../../models/user.model';
 
 const handleRequest = (userId, callback) => {
-  Users.findOne({ _id: userId }).exec((err, result) => {
-    if (err || result == null) {
-      callback(err, null);
-    } else {
-      callback(null, result);
-    }
-  });
+  Users.findOne({ _id: userId })
+    .populate('tweets')
+    .populate('retweets')
+    .exec((err, result) => {
+      if (err || result == null) {
+        callback(err, null);
+      } else {
+        callback(null, result);
+      }
+    });
 };
 
 export default {
