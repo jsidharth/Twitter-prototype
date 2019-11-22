f6829ccb05e20a63390b345fc963b0bdab295cbb
Madhu PK <54852226+MadhuPrasannaK@users.noreply.github.com>
2019-11-22 14:01:11 -0800

    Get Messages backend (#111)

---
+++ server/services/message.services/getMessages.js
@@ -0,0 +1,16 @@
+import moment from 'moment';
+import Messages from '../../models/message.model';
+
+const handleRequest = async (userId, callback) => {
+  let results = await Messages.find({ $or: [{ user_1: userId }, { user_2: userId }] })
+    .populate({ path: 'user_1', select: 'name handle profilePic ' })
+    .populate({ path: 'user_2', select: 'name handle profilePic ' });
+  if (results && results.length) {
+    results = results.sort((second, first) => moment(second.created_at).diff(first.created_at));
+  }
+  callback(null, results);
+};
+
+export default {
+  handleRequest,
+};
