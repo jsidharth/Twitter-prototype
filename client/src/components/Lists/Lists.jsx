ec6549906a87e5a6efd45518c52e514675c893b2
Tina Aggarwal <tina.14a@gmail.com>
2019-11-23 12:44:59 -0800

    removed extra css

--- client/src/components/Lists/Lists.jsx
+++ client/src/components/Lists/Lists.jsx
@@ -12,7 +12,7 @@ class Lists extends Component {
   render() {
     return (
       <div className="flexHomeScreen">
-        <div className="sideBarWidths">
+        <div>
           <Sidebar />
         </div>
         <Card className="cardWidth">
