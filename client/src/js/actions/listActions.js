d10450503e22845ff7bc87a6675946535376b2b3
tinaaggarwal <tina.14a@gmail.com>
2019-11-30 17:51:27 -0800

    Add new list modal (#183)

--- client/src/js/actions/listActions.js
+++ client/src/js/actions/listActions.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 import axios from 'axios';
 import actionTypes from '../constants/index';
+import { toast } from 'react-toastify';
 import { ROOT_URL } from '../../constant/constant';
 
 export const getLists = payload => {
@@ -27,6 +28,25 @@ export const getListDetails = payload => {
         dispatch({
           type: actionTypes.GET_LIST_DETAILS,
           payload: response.data,
+        });
+      }
+    });
+  };
+};
+
+export const createList = payload => {
+  return dispatch => {
+    console.log(payload);
+    return axios.post(`${ROOT_URL}/list/create`, payload).then(response => {
+      console.log('Status Code : ', response.status);
+      if (response.status === 200) {
+        toast.info(response.data.message, {
+          position: 'bottom-center',
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
         });
       }
     });
