a2425e132595a6bfabf0682f8b34e4d38c7ba6f0
Sidharth J <sidharthjayaprakash93@gmail.com>
2019-12-01 20:28:12 -0800

    Active flag check applied

--- server/services/tweet.services/getBookmarks.js
+++ server/services/tweet.services/getBookmarks.js
@@ -1,11 +1,14 @@
 /* eslint-disable no-underscore-dangle */
 import moment from 'moment';
+import _ from 'lodash';
 import Promise from 'bluebird';
 import Users from '../../models/user.model';
 import Tweets from '../../models/tweet.model';
 
 const handleRequest = async (userId, callback) => {
-  const user = await Users.findById(userId).populate('bookmarks');
+  const user = await Users.findById(userId)
+    .populate('bookmarks')
+    .lean();
   if (!user) {
     callback(
       {
@@ -22,24 +25,32 @@ const handleRequest = async (userId, callback) => {
             tweets: tweet._id,
           },
           {
+            _id: 1,
             name: 1,
             handle: 1,
+            active: 1,
+            profilePic: 1,
           }
         );
-        return {
-          _id: tweet._id,
-          name: tweetUser.name,
-          handle: tweetUser.handle,
-          likes_count: tweet.likes.length || 0,
-          comments_count: tweet.comments.length || 0,
-          retweet_count: tweet.retweets.length || 0,
-          body: tweet.body,
-          image: tweet.image,
-          created_at: tweet.createdAt,
-          likes: tweet.likes,
-        };
+        if (tweetUser.active) {
+          return {
+            _id: tweet._id,
+            name: tweetUser.name,
+            handle: tweetUser.handle,
+            likes_count: tweet.likes.length || 0,
+            comments_count: tweet.comments.length || 0,
+            retweet_count: tweet.retweets.length || 0,
+            body: tweet.body,
+            image: tweet.image,
+            created_at: tweet.createdAt,
+            likes: tweet.likes,
+            profilePic: tweetUser.profilePic,
+            userId: tweetUser._id,
+          };
+        }
+        return '';
       });
-
+      updatedTweets = _.compact(updatedTweets);
       updatedTweets = updatedTweets.sort((first, second) =>
         moment(second.created_at).diff(first.created_at)
       );
